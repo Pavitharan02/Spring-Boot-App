fa785dd76c6e1bbd6242c5414be67c07399f97bb
pavit <pavitharan2020@gmail.com>
2023-01-22 00:09:16 +0530

    Modified controller/UserResource.java file

---
+++ src/main/java/com/nimbusventure/routeoptimizer/UserResource.java
@@ -0,0 +1,43 @@
+package com.nimbusventure.routeoptimizer;
+
+import com.nimbusventure.routeoptimizer.model.User;
+import com.nimbusventure.routeoptimizer.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.findAllUser();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User newUser = userService.addUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        User updateUser = userService.updateUser(user);
+        return new ResponseEntity<>(updateUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{user_id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("user_id") Long user_id){
+        userService.deleteUser(user_id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
